36443a9ac1d83bd9215251479d411f99fa944951
zry <2399277159@qq.com>
2022-08-25 18:44:30 +0800

    tabbar

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-
-
+  transpileDependencies: true,
+  configureWebpack:{
+    resolve:{
+      alias:{
+        /*@已经配置-在module的配置文件*/
+        'assets':'@/assets',
+        'network':'@/network',
+        'common':'@/common',
+        'views':'@/views',
+        'components':'@/components',
+      }
+    }
+  }
 })
